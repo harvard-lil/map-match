cca1facde3c63c883fb7bca0d2f609e16e7fa71e
Annie Cain <anniejocain@gmail.com>
2015-12-16 12:12:57 -0500

    display leaderboard on game finish

--- leaderboard.php
+++ leaderboard.php
@@ -12,6 +12,16 @@ connect_db();
     $addScore  = "INSERT INTO `leaderboard` (`name`, `time`, `clicks`) VALUES ('$name','$time','$clicks')";
 
     mysql_query($addScore) or die(mysql_error());
+    
+    $retrieve = mysql_query("SELECT * FROM `leaderboard` ORDER BY `time` ASC limit 5") or die(mysql_error());
+    $item_details = array();
+    while($row = mysql_fetch_assoc($retrieve)) {
+        $item_details[] = $row;
+    }
+    
+    mysql_close($con);
+	header('Content-Type: application/json');
+    echo json_encode(array('items' => $item_details));
 
   mysql_close();
   
